3519c308682c47c67117a6d1702d83745e434db7
Sumit Gupta <sgupta98mnit@gmail.com>
2024-11-27 18:51:52 -0500

    Responding to multiple ping commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.stream.Collectors;
+
 
 public class Main {
   public static void main(String[] args){
@@ -12,13 +19,21 @@ public class Main {
         Socket clientSocket = null;
         int port = 6379;
         try {
-          serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-          clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            serverSocket = new ServerSocket(port);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            clientSocket = serverSocket.accept();
+            InputStream inputStream = clientSocket.getInputStream();
+            String result = new BufferedReader(new InputStreamReader(inputStream))
+                    .lines().collect(Collectors.joining("\n"));
+            String[] commmands = StringUtils.split(result, "\n");
+
+            for(String command : commmands) {
+                clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            }
+
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
