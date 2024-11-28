0f07b3ea7f26051b32d7bf76b977fabdf3c28b79
Sumit Gupta <sgupta98mnit@gmail.com>
2024-11-28 17:47:22 -0500

    Adding debug statement in EchoCommand class

--- src/main/java/command/EchoCommand.java
+++ src/main/java/command/EchoCommand.java
@@ -3,6 +3,7 @@ package command;
 public class EchoCommand implements Command {
     @Override
     public String execute(String[] args) {
+        System.out.println("Echo command executed: " + args[1]);
         return "$" + args[1].length() + "\r\n" + args[1] + "\r\n";
     }
 }
