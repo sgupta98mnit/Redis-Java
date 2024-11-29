897e1b271f8a687e10dcf177d852a2ceec9f9239
Sumit Gupta <sgupta98mnit@gmail.com>
2024-11-28 21:35:38 -0500

    Remove @Data with @Getter and @Setter

--- src/main/java/redis/RedisContext.java
+++ src/main/java/redis/RedisContext.java
@@ -1,11 +1,14 @@
 package redis;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Data
+@Getter
+@Setter
 public class RedisContext {
     private String[] args;
     private Map<String, String> cache = new HashMap<>();
