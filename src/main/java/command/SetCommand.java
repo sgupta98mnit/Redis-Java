f51cc198307f5ca7d33994dbea049f3a429e0f38
Sumit Gupta <sgupta98mnit@gmail.com>
2024-11-28 22:04:30 -0500

    Add debug statements

--- src/main/java/command/SetCommand.java
+++ src/main/java/command/SetCommand.java
@@ -3,13 +3,11 @@ package command;
 import org.apache.commons.lang3.StringUtils;
 import redis.RedisContext;
 
-import static utility.RedisUtility.transform;
-
 public class SetCommand implements Command {
 
     @Override
     public String execute(RedisContext context) {
-        context.getCache().put(transform(StringUtils.trim(context.getArgs()[1])), StringUtils.trim(context.getArgs()[2]));
+        context.getCache().put(StringUtils.trim(context.getArgs()[1]), StringUtils.trim(context.getArgs()[2]));
         System.out.println("Set command executed: " + context.getArgs()[1] + " "
                 + context.getArgs()[2] + " Map size: " + context.getCache().size());
         return "+OK\r\n";
