e1679de136b8b8aa7890fc760328c1090c12a482
Sumit Gupta <sgupta98mnit@gmail.com>
2024-11-28 22:01:46 -0500

    Fix code in SET command

--- src/main/java/command/GetCommand.java
+++ src/main/java/command/GetCommand.java
@@ -10,8 +10,8 @@ public class GetCommand implements Command {
     @Override
     public String execute(RedisContext context) {
         System.out.println("Get command executed: " + context.getArgs()[1] + " Map size: " + context.getCache().size());
-        context.getCache().forEach((key, value) -> System.out.println("Key: " + key + ", Value: " + value));
-        return context.getCache().getOrDefault(transform(StringUtils.trim(context.getArgs()[1])), "$-1\r\n");
+//        context.getCache().forEach((key, value) -> System.out.println("Key: " + key + ", Value: " + value));
+        return context.getCache().getOrDefault(StringUtils.trim(context.getArgs()[1]), "$-1\r\n");
     }
 
 }
