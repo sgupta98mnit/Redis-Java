654ebaac2f3fa1c0b63cf128ecb1dd0efc6401b9
Sumit Gupta <sgupta98mnit@gmail.com>
2024-11-28 21:55:33 -0500

    Add debug statements

--- src/main/java/command/GetCommand.java
+++ src/main/java/command/GetCommand.java
@@ -9,6 +9,7 @@ public class GetCommand implements Command {
     @Override
     public String execute(RedisContext context) {
         System.out.println("Get command executed: " + context.getArgs()[1] + " Map size: " + context.getCache().size());
+        context.getCache().forEach((key, value) -> System.out.println("Key: " + key + ", Value: " + value));
         return context.getCache().getOrDefault(transform(context.getArgs()[1]), "$-1\r\n");
     }
 
