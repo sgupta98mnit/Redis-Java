7f4bcfa1ad3845864b69529a15f86664bedabcef
Sumit Gupta <sgupta98mnit@gmail.com>
2024-11-28 21:51:22 -0500

    Add debug statements

--- src/main/java/command/SetCommand.java
+++ src/main/java/command/SetCommand.java
@@ -7,7 +7,8 @@ public class SetCommand implements Command {
     @Override
     public String execute(RedisContext context) {
         context.getCache().put(context.getArgs()[1], context.getArgs()[2]);
-        System.out.println("Set command executed: " + context.getArgs()[1] + " " + context.getArgs()[2]);
+        System.out.println("Set command executed: " + context.getArgs()[1] + " "
+                + context.getArgs()[2] + "Map size: " + context.getCache().size());
         return "+OK\r\n";
     }
 }
